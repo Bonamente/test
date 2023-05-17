d16abee01cbb7a6b755349b68b6b144c39be3339
Bonamente <56506035@protonmail.com>
2023-05-17 12:59:45 +0500

    feat(Header): update styles

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -3,12 +3,32 @@ import cn from 'classnames'
 
 import s from './header.module.scss'
 
+import Logo from 'components/Logo/Logo'
+import Navigation from 'components/Navigation/Navigation'
+import ContactPhone from 'components/ContactPhone/ContactPhone'
+import { Button } from 'components'
+
 export interface HeaderProps {
   classNames?: string
 }
 
 export const Header: FC<HeaderProps> = ({ classNames }) => {
-  return <div className={cn(s.header, classNames)}>
-      header
-  </div>
+  return (
+    <header className={cn(s.header, classNames)}>
+      <div className={cn(s.headerContent)}>
+        <Logo
+          imgUrl='/assets/icons/logo_icon.svg'
+          classNames={cn(s.headerLogo)}
+        />
+        <div className={cn(s.navWrapper)}>
+          <Navigation />
+        </div>
+        <ContactPhone />
+
+        <Button onClick={() => {}} classNames={cn(s.callbackBtn)}>
+          Перезвоните мне
+        </Button>
+      </div>
+    </header>
+  )
 }
