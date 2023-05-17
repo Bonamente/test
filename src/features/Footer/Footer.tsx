403a768ec27ebc0ea32aa68b333d4d10e673406c
Bonamente <56506035@protonmail.com>
2023-05-17 13:00:45 +0500

    feat(Footer): update styles

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -2,11 +2,23 @@ import { FC } from 'react'
 import cn from 'classnames'
 
 import s from './footer.module.scss'
+import Logo from 'components/Logo/Logo'
+import FooterContacts from 'components/FooterContacts/FooterContacts'
 
 export interface FooterProps {
   classNames?: string
 }
 
 export const Footer: FC<FooterProps> = ({ classNames }) => {
-  return <div className={cn(s.footer, classNames)}>Footer</div>
+  return (
+    <footer className={cn(s.footer, classNames)}>
+      <div className={cn(s.footerContent)}>
+        <Logo
+          imgUrl='/assets/icons/footer-logo_icon.svg'
+          classNames={cn(s.footerLogo)}
+        />
+        <FooterContacts />
+      </div>
+    </footer>
+  )
 }
