81eac2b248908785771a587935bfdf340b1018d1
Bonamente <56506035@protonmail.com>
2023-05-19 09:46:30 +0500

    feat(HomePage): update styles

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,22 @@
 import { FC } from 'react'
 
 import s from './homePage.module.scss'
+import Promo from 'components/Promo/Promo'
+import UserActions from 'components/UserActions/UserActions'
+import Survey from 'components/Survey/Survey'
+import Head from 'next/head'
 
 export const HomePage: FC = () => {
-  return <div className={s.homePage}>homepage</div>
+  return (
+    <div className={s.homePage}>
+      <Head>
+        <title>Магазин квартир</title>
+      </Head>
+      <Promo />
+      <div className={s.homePageWrapper}>
+        <UserActions />
+        <Survey />
+      </div>
+    </div>
+  )
 }
